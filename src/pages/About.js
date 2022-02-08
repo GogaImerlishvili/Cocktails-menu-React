84040cc09d2b7df912b32a61c5b4551b3adc9760
Goga <gogaimerlishvili1@gmail.com>
2022-02-08 13:21:19 +0400

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+  return (
+      <section className='section about-section'>
+          <h1 className='section-title'>About us</h1>
+          <p>They are the great American invention. And there has
+                never been a better time to drink them than now.
+               We traveled 34,462 miles and sipped 400 gin fizzes,
+               Manhattans, and Gibsons with one goal—to compile the 
+               first ranking of the nation's best cocktail bars</p>
+      </section>
+  )
+};
+
+export default About;
